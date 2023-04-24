f59e685bbc6a3825ad4e36eb23c4db227423494c
CJ-Codes <ccoreas484@gmail.com>
2023-04-23 22:53:29 -0400

    Added api request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const [data, setData] = useState({pokem:[]});
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/?limit=20')
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const result = await axios(url);
+  
+      setData(result.data);
+    }
+    fetchData();
+    
+  }, []);
+  
+    console.log(data);
+  // const card = data.results.map((object) => {
+  //   return (
+  //     <div key={object.url}>
+  //       <h3>{object.name}</h3>
+  //     </div>
+  //   )
+  // })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokedex</h1>
+      {/* {card.length > 0 ? card : "Loading..."} */}
+
+      <button>Prev</button>
+      <button type='button' onClick={() => {setUrl(data.next); }}>Next</button>
+
     </div>
   );
 }
